2021012e08308597a117cb6816e8dd1b1e7f10ad
AlaaZme <alaazme1@live.com>
2024-02-24 12:33:48 +0200

    CSS

--- load_game.py
+++ load_game.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from views import views
 
-def __main__():
+
+def main():
     app = Flask(__name__)
     app.register_blueprint(views, url_prefix='/')
-
-
-
     app.run(port=81)
 
-__main__()
+
+if __name__ == "__main__":
+    main()
